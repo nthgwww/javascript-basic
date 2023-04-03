2795c8a97ae9527b70e64debbc7c5d26fc425591
ngocthong <6051071118@st.utc2.edu.vn>
2023-04-03 16:04:25 +0700

    loops

--- compare/compare.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello world")
-let a = 5; 
-let b = '5';
-let c;
-
-console.log('compare a,b','a = ',a, 'b =', a, 'result', a==b);
-console.log('compare a, b','a = ',a, 'b =', a, 'result', a===b);    
-console.log('check value :', c)
