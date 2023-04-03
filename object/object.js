568fde86c6fce43c76550d9b25adb61199575393
ngocthong <6051071118@st.utc2.edu.vn>
2023-04-03 15:21:31 +0700

    object

---
+++ object/object.js
@@ -0,0 +1,8 @@
+let obj = {
+    name: "Tommy",
+    address:"Gia Lai"
+};
+
+let b = "Tommy";
+let c = '';
+console.log("What is your name :", obj.name);
--- variables/variables.js
+++
@@ -1,8 +0,0 @@
-console.log("hello world from javascript")
-const pi = 3.14;
-let person = "John Doe";
-let answer = 'Yes I am!';
-const price1 = 5;
-const price2 = 6;
-let total = price1 + price2;
-//let = int, float, string
