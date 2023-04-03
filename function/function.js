8231035a2f21c57686fe92c17a67481d3ef3fead
ngocthong <6051071118@st.utc2.edu.vn>
2023-04-03 16:45:37 +0700

    function

---
+++ function/function.js
@@ -0,0 +1,9 @@
+//input parameter
+
+function sum(a,b){
+    return a+b;
+}
+
+let c = sum(10,20);
+
+console.log("Sum a+b = :", sum(6,9));
--- loops/loops.js
+++
@@ -1,5 +0,0 @@
-let arr = ['MU', 'MC', "Arsenal", "Barca"]
-
-for (let i=0; i<arr.length; i++){
-    console.log('check variable', arr[i]);  
-}
