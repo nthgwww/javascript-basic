a806c68e7bf8580b459409cab4d5aa8ef73d9293
ngocthong <6051071118@st.utc2.edu.vn>
2023-04-03 15:48:55 +0700

    compare

--- array/array.js
+++
@@ -1,6 +0,0 @@
-let family = [
-    { name : "Tommy", age: "18"},
-    { name : "TommyHo", age: "25"},
-]
-
-console.log("my famiy", family);
---
+++ compare/compare.js
@@ -0,0 +1,8 @@
+console.log("Hello world")
+let a = 5; 
+let b = '5';
+let c;
+
+console.log('compare a,b','a = ',a, 'b =', a, 'result', a==b);
+console.log('compare a, b','a = ',a, 'b =', a, 'result', a===b);    
+console.log('check value :', c)
